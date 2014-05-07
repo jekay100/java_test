3e1c854db2d5ed336c38c9ec2d48523491c01446
David Wong <trilever31204@163.com>
2014-05-07 17:28:01 +0800

    Update managerinheritanceTestDrive.java
    向下造型的使用。将向下造型的结果还原回去。而不能将非向下造型的结果父类变量进行向下造型。

--- managerinheritanceTestDrive.java
+++ managerinheritanceTestDrive.java
@@ -4,7 +4,7 @@
     > Version: 1.0
     > Mail: trilever31204@gmail.com 
     > Created Time: 2014-5-7 10:14:33
-    > The program demonstrates  and tansformation
+    > The program demonstrates and tansformation
  ************************************************************************/
 import java.util.*;
 
@@ -80,10 +80,10 @@ public class managerinheritanceTestDrive
 {
 	public static void main(String[] args)
 	{
-		Manager b=new Manager("cal",80000,1987,12,15);
-		Emploree e=b;
-		Manager boss=(Manager)e;
-		boss.setBonus(5000);
+		Manager b=new Manager("cal",80000,1987,12,15);//这样建立一个子类变量k
+		Emploree e=b//这样是向上造型，将子类对象转换为父类对象，这样可以实现多态，但是转换后的父类变量e并不能调用子类中独有的方法f（），只能调用父子类都有的方法。
+		Manager boss=(Manager)e//这样就是向下造型。类型强制转换。将向上造型获得的父类变量还原成他原来的类型。
+		boss.setBonus(5000//这样可以通过向下造型获得的子类对象来调用子类独有的方法。
 		System.out.println("name:"+boss.getName()+"salary:"+boss.getSalary());
 	}
 }
