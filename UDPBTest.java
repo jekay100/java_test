1c8be2eb1e2f098c69376002746b2ecdca665d8a
trilever <trilever31204@163.com>
2014-07-10 21:25:23 +0800

    add udp communication

---
+++ UDPBTest.java
@@ -0,0 +1,38 @@
+/**
+ * @file Name: UDPBTest.java
+ * @description:Test the UDPBTest class
+ * @author : trilever
+ * @version : 1.0
+ * @mail: trilever31204@gmail.com 
+ * @created Time: 2014-7-10 17:21:00
+ */
+import java.net.*;
+import java.io.*; 
+public class UDPBTest
+{
+	final static int Server_PORT=30000;
+	public static void main(String[] args) 
+	{
+		try
+		{
+			//construct client's DatagramSocket without specific port
+			DatagramSocket dgs = new DatagramSocket();
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String content = null;
+			while((content=br.readLine())!=null)
+			{
+				InetAddress iaa = InetAddress.getByAddress(new byte[]{10,23,96,124});
+				//construct a DatagramPacket, it is aimed at storing the data which will be transmitted by DatagramSocket,with server's address and port.
+				DatagramPacket dgp = new DatagramPacket(content.getBytes(),content.length(),iaa,Server_PORT); 
+				//send the data
+				dgs.send(dgp); 
+			}
+			
+		}
+		
+		catch(Exception e)
+		{
+			System.out.println("error");
+		}
+	}
+}
